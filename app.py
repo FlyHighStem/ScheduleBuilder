6e27208896994746d7a19d9c2d456e7789591833
Munkhuu1107 <oyuntsetseg1214@gmail.com>
2026-04-18 23:13:06 -0700

    Save my localchanges

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
-print "hello";
+from fastapi import FastAPI
+from fastapi.responses import FileResponse
+
+app = FastAPI()
+
+# This tells FastAPI: "When someone visits the home page (/), run this function"
+@app.get("/")
+def serve_dashboard():
+    # This points directly to the HTML file in your Frontend folder
+    return FileResponse("Frontend/homepage.html")
